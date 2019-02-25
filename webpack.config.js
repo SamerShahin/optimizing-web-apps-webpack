24923ee96c2341893d2225a9edcfa6e023ecfd20
SamerShahin <samer.shahin@galilsoftware.com>
2019-02-25 17:58:09 +0200

    webpack hot reaload

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: "development",
     entry: './app/app.js',
     output: {
         path: path.resolve(__dirname, 'app/dist'),
-        filename: "app.bundle.js"
+        filename: "app.bundle.js",
+        publicPath: "/dist/"
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'app'),
-        publicPath: '/dist/'
-    }
+        publicPath: '/dist/',
+        watchContentBase: false,
+        // hotOnly: true
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
